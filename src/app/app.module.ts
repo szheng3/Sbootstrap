f52f48914c8f379358dde1b08e4ec96d5e70163e
Shuai <szheng3@uci.edu>
2019-08-09 00:07:40 -0500

    basic typography

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicTypographyComponent } from './basic-typography/basic-typography.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicTypographyComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
