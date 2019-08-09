dc9bfd4e5eb1c68fb17fee2bbbb59d68fa6a2ccc
Shuai <szheng3@uci.edu>
2019-08-09 09:34:41 -0500

    basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {BasicTypographyComponent} from 'src/app/basic-typography/basic-typography.component';
 
 const routes: Routes = [
-
+  {
+    path: 'basic-typography', component: BasicTypographyComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
