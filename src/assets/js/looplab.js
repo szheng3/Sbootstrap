b7ccc17457c56fbf30df276eb762509937ff0953
Shuai <szheng3@uci.edu>
2019-08-12 15:46:12 -0500

    added nav

--- src/assets/js/looplab.js
+++ src/assets/js/looplab.js
@@ -1,2 +1,17 @@
 $('#year').text(new Date().getFullYear());
-console.log(33)
+$('body').scrollspy({ target: '#main-nav' });
+// Smooth Scrolling
+$("#main-nav a").on('click', function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function () {
+
+      window.location.hash = hash;
+    });
+  }
+});
