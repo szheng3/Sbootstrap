380949522a43349b25e7f2d8b7f315b8723c0ec2
Shuai <szheng3@uci.edu>
2019-08-26 21:05:09 -0500

    added dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,11 +5,19 @@ import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardComponent} from 'src/app/dashboard/dashboard.component';
 import {ShareModule} from 'src/app/share/share.module';
 import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { TableComponent } from './table/table.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     NavComponent,
+    HeaderComponent,
+    SearchComponent,
+    TableComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
